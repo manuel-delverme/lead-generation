6c815f48df199f1e9414d35344866919c99d98c6
Yitef <man@themoon.net>
2015-12-28 13:53:34 -0500

    keyword extractor works

--- keywords.py
+++ keywords.py
@@ -6,21 +6,29 @@ import json
 import shelve
 import collections
 
-
 class Classifier(object):
     def __init__(self):
-        self.wordfreqs = shelve.open("/tmp/wordfreq")
+        self.wordfreqs = shelve.open("/tmp/wordfreq", flag="n")
         self.stemmer = PorterStemmer()
+        self.stopwords = set(nltk.corpus.stopwords.words('italian'))
+        self.stopwords.union(nltk.corpus.stopwords.words('english'))
+
         with open("enwiki-20150602-words-frequency.txt") as f:
             for wordfreq in f:
                 key, freq = wordfreq[:-1].split()
-                key = self.stemmer.stem(key)
+                if(is_unicode(key)):
+                    break
+                freq = float(freq)
+                key = str(self.stemmer.stem(key))
                 try:
                     self.wordfreqs[key] += freq
                 except KeyError:
                     self.wordfreqs[key] = freq
 
+
     def extract_keywords(self, text):
+        if ''  == text:
+            return collections.OrderedDict([])
         words = self.transform(text)
         fdist = nltk.FreqDist(words)
         result = dict()
@@ -29,7 +37,9 @@ class Classifier(object):
                 result[word] = fdist[word] / self.wordfreqs[word]
 
         # normalize
-        max_val = max(result.values())
+        if len(result) > 0:
+            max_val = max(result.values())
+
         for word in fdist:
             if word not in result:
                 result[word] = 1.1
@@ -38,23 +48,29 @@ class Classifier(object):
 
         return collections.OrderedDict(sorted(result.items(), key=lambda k:-k[1]))
 
-    def transform(text, stem=True):
-        stopwords = set(nltk.corpus.stopwords.words('italian'))
-        stopwords.union(nltk.corpus.stopwords.words('english'))
-
+    def transform(self, text, stem=True):
         tokens = nltk.word_tokenize(text)
         filtered_tokens = list()
         for token in tokens:
-            if token not in string.punctuation and token not in stopwords:
+            if token not in string.punctuation and token not in self.stopwords:
                 filtered_tokens.append(token.lower())
 
         if stem:
             stemmed = []
             for item in filtered_tokens:
-                stemmed.append(self.stemmer.stem(item))
+                stemmed.append(str(self.stemmer.stem(item)))
             return stemmed
         else:
             return filtered_tokens
+
+def is_unicode(word):
+    try:
+        word.decode('ascii')
+        return False
+    except UnicodeDecodeError:
+        return True
+    except UnicodeEncodeError:
+        return True
 
 if __name__ == '__main__':
     c = Classifier()
@@ -63,4 +79,10 @@ if __name__ == '__main__':
             row = json.loads(row)
             classification = row['dmoz_url'].split("/")[5:]
             text = " ".join(row['metadata'])
-            print c.extract_keywords(row[:-1]), classification
+            if(is_unicode(text)):
+                continue
+            text = text.decode('utf8')
+            print "target:", classification
+            for keyword,value in c.extract_keywords(text).items():
+                print keyword, value
+            raw_input()
