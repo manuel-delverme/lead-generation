f382b368e4f91bd6c6f28d2a7ad453506144bc5a
Manuel <manuel.delverme@gmail.com>
2016-03-12 16:32:58 +0100

    dropped scrapy, and threads

--- custom_scrapers/m.paginegialle_scraper/m.paginegialle.py
+++ custom_scrapers/m.paginegialle_scraper/m.paginegialle.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-import threading
+from __future__ import print_function
 import time
 import datetime
 import random
 import json
+
+import itertools
+
 import settings
 import requests
 import collections
@@ -31,117 +34,93 @@ def get_last_crawled_page():
             if line is None:
                 raise IOError
         last_page = int(line)
-        print datetime.datetime.now(), "resuming from ", last_page
+        print(datetime.datetime.now(), "resuming from ", last_page)
     except IOError:
         last_page = 0
-        print datetime.datetime.now(), "resuming from ", last_page, "no progress.log found"
+        print(datetime.datetime.now(), "resuming from ", last_page, "no progress.log found")
     return last_page
 
 
-class PagineGialleSpider(CrawlSpider):
+class PagineGialleSpider():
     name = "m.paginegialle.it"
-    handle_httpstatus_list = [200]
 
-    def __init__(self, *a, **kw):
-        super(PagineGialleSpider, self).__init__(*a, **kw)
-        self.MAX_PAGESIZE = 1000
+    def __init__(self):
+        self.api_urls = [
+            # "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&what=%00&page={}&_={}",
+            # "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&address=%00&page={}&_={}"
+            "http://mobile.seat.it/searchpg?pagesize={}&where=Italia&sortby=name&output=jsonp&page={}&_={}",
+        ]
+        self.cookies = { 's_vi': '[CS]v1|2AF274BE0531254E-400001050000BEF7[CE]', }
+        self.headers = { 'Pragma': 'no-cache', 'DNT': '1', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'en-US,en;q=0.8,it;q=0.6', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36', 'Accept': '*/*', 'Referer': 'http://m.paginegialle.it/listing?what=formaggio&where=Italia', 'Connection': 'keep-alive', 'Cache-Control': 'no-cache', }
+        self.MAX_PAGESIZE = 99999
         self.MIN_PAGESIZE = 80
-        self.parsed_items = None
-        self.failed_requests = collections.deque([])
+        self.pagesize = 999
+        self.pg_db_entries = 3541693
+        self.nr_parsed_items = 0
 
     def page_nr_iter(self, already_parsed_items):
-        self.parsed_items = already_parsed_items
-
-        with open("progress.log", 'a') as plog:
-            while True:
-                page_size = min(max(self.parsed_items, self.MIN_PAGESIZE), self.MAX_PAGESIZE)
-                page_nr = int(self.parsed_items / page_size)
-                # retry the failed requests
-                try:
-                    failed_request = self.failed_requests.pop()
-                    page_nr = failed_request.meta['pg_page_nr']
-                    yield page_nr
-                except IndexError:
-                    break
-            plog.write(str(page_nr) + "\n")
-            plog.flush()
-            yield (page_nr, page_size)
-
-    def start_requests(self):
-        search_url = "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&what=%00&page={}&_={}"
-
-        rnd = random.randrange(1000000000, 9999999999)
-        print "downloading pg info"
-
+        # controls request progress and calculates what to download next
+        self.nr_parsed_items = already_parsed_items
         while True:
-            time.sleep(settings.DOWNLOAD_DELAY)
-            initial_response = requests.get(search_url.format(25, 1, rnd))
-            try:
-                statistics_json = json.loads(initial_response.text[1:-3])
-            except ValueError as e:
-                print initial_response.text
-                print "err", e
-                time.sleep(60 * 5)
-                continue
-            err, err_msg = self._verify_response(statistics_json)
-            if not err:
-                break
-            else:
-                self._fail(initial_response, err_msg, retry=False)
+            page_size = min(max(self.nr_parsed_items, self.MIN_PAGESIZE), self.MAX_PAGESIZE)
+            page_nr = int(self.nr_parsed_items / page_size)
+            yield (page_size, page_nr)
 
-        already_parsed_items = get_last_crawled_page()
-        # last_page = statistics_json['lastPage']
-        # print "found {} pages".format(last_page)
-        print "starting from {}".format(already_parsed_items)
+    def call_api(self, page_size, page):
+        # fetches api json object
+        while True:
+            rnd = random.randrange(1000000000, 9999999999)
+            time.sleep(settings.DOWNLOAD_DELAY)
+            url = random.choice(self.api_urls)
+            http_response = requests.get(url.format(page_size, page, rnd), headers=self.headers, cookies=self.cookies)
+            try:
+                json_response = json.loads(http_response.text[1:-3])
+            except ValueError as e:
+                # most likely error 500
+                print(http_response.text, "err", e)
+                time.sleep(60 * 5)
+            else:
+                self.pg_db_entries = max(self.pg_db_entries, json_response['resultsNumber'])
+                err, err_msg = self._verify_response(json_response)
+                if not err:
+                    return json_response
+                else:
+                    print("failed", (page, page_size), err_msg)
+
+    def generate_requests(self):
+        self.nr_parsed_items = get_last_crawled_page()
+
+        already_parsed_pages = self.nr_parsed_items / self.pagesize
+        print("starting from {}".format(self.nr_parsed_items))
 
         # main loop
-        for page_nr, page_size in self.page_nr_iter(already_parsed_items):
-            rnd = random.randrange(10000000000, 99999999999)
+        for page_nr in itertools.count(already_parsed_pages):
+            yield (self.pagesize, page_nr)
 
-            req = scrapy.http.request.Request(search_url.format(page_size, page_nr, rnd), callback=self.parse_search)
-            req.meta['pg_page_nr'] = page_nr
-            yield req
+        for page_size, page_nr in self.page_nr_iter(self.nr_parsed_items):
+            yield (page_size, page_nr)
 
-    def parse_search(self, http_response):
-        search_results = json.loads(http_response.body[1:-3])
+    def parse_pg_search(self, search_results):
+        # turns json object in items
+        perc = 100 * (self.nr_parsed_items / self.pg_db_entries)
+        print(datetime.datetime.now(), "progress:", self.nr_parsed_items, "results:", len(search_results['results']), "/", search_results['pagesize'], perc, "%")
 
-        err, err_msg = self._verify_response(search_results)
-        if err:
-            self._fail(http_response, err_msg)
-            yield None
-        else:
-            perc = 100 * ((search_results['currentPage'] * search_results['pagesize']) / search_results['resultsNumber'])
-            print datetime.datetime.now(), search_results['currentPage'], "-> results:", len(
-                search_results['results']), perc, "%"
-
-            for search_result in search_results['results']:
-                item = Business()
-
-                mapping = {'phones': 'phones', 'addresses': 'address', 'cities': 'city', 'countries': 'country',
-                           'emails': 'emailAddress', 'pg_id': 'id', 'name': 'name', 'province': 'province',
-                           'homepage': 'webAddress', 'zip': 'zip'}
-                self.parsed_items += 1
-                for k, v in mapping.items():
-                    item[k] = json.dumps(get_or_default(search_result, v, ''))
-                yield item
-
-    def _fail(self, failed_response, msg, retry=True):
-        if hasattr(failed_response.request, 'meta'):
-            resp_id = failed_response.request.meta['pg_page_nr']
-        else:
-            resp_id = "NO ID FOUND"
-        print datetime.datetime.now(), resp_id, msg
-        if retry:
-            failed_request = scrapy.http.request.Request(failed_response.url, callback=self.parse_search,
-                                                         meta={
-                                                             'pg_page_nr': failed_response.request.meta['pg_page_nr']})
-            self.failed_requests.append(failed_request)
+        for search_result in search_results['results']:
+            item = Business()
+            mapping = {'phones': 'phones', 'addresses': 'address', 'cities': 'city', 'countries': 'country', 'emails': 'emailAddress', 'pg_id': 'id', 'name': 'name', 'province': 'province', 'homepage': 'webAddress', 'zip': 'zip'}
+            self.nr_parsed_items += 1
+            for k, v in mapping.items():
+                item[k] = json.dumps(get_or_default(search_result, v, ''))
+            yield item
+        with open("progress.log", 'a') as plog:
+            plog.write(str(self.nr_parsed_items) + "\n")
+            plog.flush()
 
     @staticmethod
     def _verify_response(search_results):
         if 'resultsNumber' not in search_results:
             return True, "resultsNumber not found"
-        elif search_results['resultsNumber'] < 1000000: #3000000:
+        elif search_results['resultsNumber'] < 3000000/search_results['pagesize']:
             return True, "{} results, too few".format(search_results['resultsNumber'])
         elif 'results' not in search_results:
             return True, "results not found"
@@ -149,34 +128,20 @@ class PagineGialleSpider(CrawlSpider):
             return True, "found {} results expected {}".format(len(search_results['results']),
                                                                search_results['pagesize'])
         else:
-            print "search_results['resultsNumber']", search_results['resultsNumber']
-            print "found {} results expected {}".format(len(search_results['results']), search_results['pagesize'])
             return False, ""
 
-    @staticmethod
-    def parse_details(json_entry):
-        details_results = json.loads(json_entry.body[1:-3])['detail']
-        item = Business()
 
-        item['pg_id'] = json.dumps(details_results['id'])
-        item['meta_description'] = json.dumps(details_results['description'])
+def main():
+    spider = PagineGialleSpider()
+    pipeline = DatabasePipeline()
 
-        yield item
-
-spider = PagineGialleSpider()
-pipeline = DatabasePipeline()
-
-for request in spider.start_requests():
-    result = request
-
-    while isinstance(result, scrapy.http.request.Request):
-        time.sleep(settings.DOWNLOAD_DELAY)
-        response = requests.get(request.url)
-        response = scrapy.http.HtmlResponse(body=response.text, request=request, encoding="UTF-8", url=response.url)
-        for result in request.callback(response):
-            if isinstance(result, Business):
-                pipeline.process_item(result)
-            elif result is None:
+    for page_size, page_nr in spider.generate_requests():
+        result_json = spider.call_api(page_size, page_nr)
+        for businessEntry in spider.parse_pg_search(result_json):
+            if isinstance(businessEntry, Business):
+                pipeline.process_item(businessEntry)
+            elif businessEntry is None:
                 pass
             else:
                 raise Exception("unhandled response")
+main()
