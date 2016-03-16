73a1348820eba37f1a9e2dedc7569570b330b637
Yitef <man@themoon.net>
2016-03-16 12:42:10 -0400

    testing entrypoint limits

--- custom_scrapers/m.paginegialle_scraper/m.paginegialle.py
+++ custom_scrapers/m.paginegialle_scraper/m.paginegialle.py
@@ -34,14 +34,13 @@ def get_last_crawled_item():
                 pass
             if line is None:
                 raise IOError
-        last_page = int(line)
-        page_nr, category = line.split(",")[:-1]
+        page_nr, category = line[:-1].split(",")
         page_nr = int(page_nr)
-        print(datetime.datetime.now(), "resuming from ", last_page)
+        print(datetime.datetime.now(), "resuming from ", page_nr)
     except IOError:
-        page_nr = 0
-        category = ""
-        print(datetime.datetime.now(), "resuming from ", last_page, "no progress.log found")
+        page_nr = 1
+        category = "none"
+        print(datetime.datetime.now(), "resuming from ", page_nr, "no progress.log found")
     return page_nr, category
 
 
@@ -53,15 +52,13 @@ class PagineGialleSpider(object):
             # "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&what=%00&page={}&_={}",
             # "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&address=%00&page={}&_={}"
             # "http://mobile.seat.it/searchpg?pagesize={}&where=Italia&sortby=name&output=jsonp&page={}&_={}",
-            "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&lang=en&categories={}&_={}"
+            "http://mobile.seat.it/searchpg?client=pgbrowsing&version=5.0.1&device=evo&pagesize={}&output=jsonp&lang=en&categories={}&page={}&_={}"
         ]
         self.cookies = { 's_vi': '[CS]v1|2AF274BE0531254E-400001050000BEF7[CE]', }
         self.headers = { 'Pragma': 'no-cache', 'DNT': '1', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'en-US,en;q=0.8,it;q=0.6', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36', 'Accept': '*/*', 'Referer': 'http://m.paginegialle.it/listing?what=formaggio&where=Italia', 'Connection': 'keep-alive', 'Cache-Control': 'no-cache', }
-        self.MAX_PAGESIZE = 99999
-        self.MIN_PAGESIZE = 80
-        self.pagesize = 999
-        self.pg_db_entries = 3541693
-        self.nr_parsed_items = 0
+        self.pagesize = 4300
+        # self.pg_db_entries = 3541693
+        # self.nr_parsed_items = 0
 
     def page_nr_iter(self, already_parsed_items):
         # controls request progress and calculates what to download next
@@ -71,13 +68,13 @@ class PagineGialleSpider(object):
             page_nr = int(self.nr_parsed_items / page_size)
             yield (page_size, page_nr)
 
-    def call_api(self, page_size, page):
+    def call_api(self, page_size, category, page):
         # fetches api json object
         while True:
             rnd = random.randrange(1000000000, 9999999999)
             time.sleep(settings.DOWNLOAD_DELAY)
             url = random.choice(self.api_urls)
-            http_response = requests.get(url.format(page_size, page, rnd), headers=self.headers, cookies=self.cookies)
+            http_response = requests.get(url.format(page_size, category, page, rnd), headers=self.headers, cookies=self.cookies)
             try:
                 json_response = json.loads(http_response.text[1:-3])
             except ValueError as e:
@@ -85,7 +82,7 @@ class PagineGialleSpider(object):
                 print(http_response.text, "err", e)
                 time.sleep(60 * 5)
             else:
-                self.pg_db_entries = max(self.pg_db_entries, json_response['resultsNumber'])
+                # self.pg_db_entries = max(self.pg_db_entries, json_response['resultsNumber'])
                 err, err_msg = self._verify_response(json_response)
                 if not err:
                     return json_response, err_msg
@@ -93,37 +90,41 @@ class PagineGialleSpider(object):
                     print("failed", (page, page_size), err_msg)
 
     def generate_requests(self):
-        page_nr, category = get_last_crawled_item()
+        page_nr, resumed_category = get_last_crawled_item()
+        self.category = resumed_category
 
         # already_parsed_pages = self.nr_parsed_items / self.pagesize
-        print("starting from {} / {}".format(category, page_nr))
+        print("starting from {}/{}".format(page_nr, resumed_category))
 
         with open("categories.txt") as categories_list:
-            if category != "":
-                while category != next(categories_list):
+            if resumed_category != "none":
+                while resumed_category != next(categories_list)[:-1]:
+                    #scroll the file until we get our category
                     pass
                 for i in itertools.count(page_nr):
-                    message = yield (self.pagesize, category)
-                    if type(message, StopIteration):
+                    #finish yielding page numbers
+                    message = yield (self.pagesize, resumed_category, i)
+                    if isinstance(message, StopIteration):
                         break
 
             # main loop
-            for category in categories_list
-                self.categoriy = category
+            for category in categories_list:
+                category = category[:-1]
+                self.category = category
                 for i in itertools.count(1):
-                    message = yield (self.pagesize, category)
-                    if type(message, StopIteration):
+                    message = yield (self.pagesize, category, i)
+                    if isinstance(message, StopIteration):
                         break
 
     def parse_pg_search(self, search_results):
         # turns json object in items
-        perc = (100 * self.nr_parsed_items ) / self.pg_db_entries
-        print(datetime.datetime.now(), "progress:", self.nr_parsed_items, "results:", len(search_results['results']), "/", search_results['pagesize'], perc, "%")
+        # perc = (100 * self.nr_parsed_items ) / self.pg_db_entries
+        print(datetime.datetime.now(), "category:", self.category, "page:", search_results['currentPage'], "results:", len(search_results['results']), "/", search_results['pagesize'])
 
         for search_result in search_results['results']:
             item = Business()
             mapping = {'phones': 'phones', 'addresses': 'address', 'cities': 'city', 'countries': 'country', 'emails': 'emailAddress', 'pg_id': 'id', 'name': 'name', 'province': 'province', 'homepage': 'webAddress', 'zip': 'zip'}
-            self.nr_parsed_items += 1
+            # self.nr_parsed_items += 1
             for k, v in mapping.items():
                 item[k] = json.dumps(get_or_default(search_result, v, ''))
             yield item
@@ -131,17 +132,22 @@ class PagineGialleSpider(object):
     @staticmethod
     def _verify_response(search_results):
 
-        required_keys = ['resultsNumber', 'results', 'current_page', 'pagesize']
+        required_keys = ['resultsNumber', 'results', 'currentPage', 'pagesize']
         for key in required_keys:
             if key not in search_results:
                 return True, "{} not found".format(key)
 
-        page_end = search_results['current_page'] * search_results['pagesize']
-        if  page_end > search_results['max_results']:
-            expected_results = current_page * page_size - max_results
+        current_page = search_results['currentPage']
+        page_size = search_results['pagesize']
+        max_results = search_results['resultsNumber']
+
+        page_end = current_page * page_size
+        if  page_end > max_results:
+            expected_results = page_end - max_results
             last_page = True
         else:
-            expected_results = search_results['pagesize']
+            expected_results = page_size
+            last_page = False
 
         if len(search_results['results']) != expected_results:
             return True, "found {} results expected {}".format(len(search_results['results']), expected_results)
@@ -157,18 +163,25 @@ def main():
     pipeline = DatabasePipeline()
 
     request_generator = spider.generate_requests()
-    for page_size, page_nr in request_generator:
-        result_json, err_msg = spider.call_api(page_size, page_nr)
+    scraped_ids = set()
+    for page_size, category, page_nr in request_generator:
+        result_json, err_msg = spider.call_api(page_size, category, page_nr)
         for businessEntry in spider.parse_pg_search(result_json):
             if isinstance(businessEntry, Business):
-                pipeline.process_item(businessEntry)
+                if businessEntry['pg_id'] in scraped_ids:
+                    import ipdb; ipdb.set_trace()
+                    print("duplicate")
+                    break
+                else:
+                    scraped_ids.add(businessEntry['pg_id'])
+                    pipeline.process_item(businessEntry)
             elif businessEntry is None:
                 pass
             else:
                 raise Exception("unhandled response")
 
         with open("progress.log", 'a') as plog:
-            plog.write(str(page_nr)+ "," self.category + "\n")
+            plog.write(str(page_nr) + "," + spider.category + "\n")
             plog.flush()
         if err_msg == "last_page":
             request_generator.send(StopIteration)
