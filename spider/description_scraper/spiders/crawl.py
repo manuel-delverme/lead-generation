76c6298b889a728ba856d013748d6e1b78c95a22
Yitef <man@themoon.net>
2016-03-25 15:47:52 -0400

    added url generator from db

--- spider/description_scraper/spiders/crawl.py
+++ spider/description_scraper/spiders/crawl.py
@@ -1,39 +1,76 @@
 # -*- coding: utf-8 -*-
-import logging
 import scrapy
 import json
 import scrapy.loader
+import psycopg2
+import psycopg2.extras
 from description_scraper.items import Business
 
 
 class CrawlSpider(scrapy.Spider):
-    name = "crawl"
-    # allowed_domains = ["www.dmoz.org"]
-    start_urls = (
-        'http://www.dmoz.org/World/Italiano/Affari/',
-    )
+    name = "website_crawler"
 
-    def parse(self, response):
+    def __init__(self, **kwargs):
+        super(CrawlSpider, self).__init__(**kwargs)
+        connect_string = "dbname={} user={} host={} password={}".format("grepr", "spider", "tuamadre.net", "write_only")
+        self._conn = psycopg2.connect(connect_string)
 
-        hrefs = response.css("ul.directory-url > li > a::attr('href')")
-        for href in hrefs:
-            url = response.urljoin(href.extract())
+    def start_requests(self):
+        with self._conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+            # cursor.execute('select * from "Businesses" where homepage != $$""$$ limit 100')  # crawl_time is NULL')
+            cursor.execute('select DISTINCT homepage from "Businesses" where homepage != $$""$$ limit 100')  # crawl_time is NULL')
+            for db_entry in cursor:
+                homepage = db_entry['homepage']
+                try:
+                    homepage = json.loads(db_entry['homepage'])
+                except ValueError:
 
-            request = scrapy.Request(url, callback=self.parse_homepage)
-            request.meta['dmoz_url'] =  response.url
-            yield request
+                    if homepage[0] == '"' and homepage[-1] == '"':
+                        homepage = homepage[1:-1]
+                    elif homepage[0] == '{' and homepage[-1] == '}':
+                        homepage = homepage[1:-1]
 
-        sub_directories = response.css("ul.directory.dir-col > li > a::attr('href')")
-        for href in sub_directories:
-            url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse)
+                    if "," in homepage:
+                        entry_urls = homepage.split(",")
+                    else:
+                        entry_urls = [homepage]
+                else:
+                    if type(homepage) in (unicode, str):
+                        entry_urls = [homepage]
+                    elif type(homepage) == list:
+                        pass
+                    else:
+                        import ipdb
+                        ipdb.set_trace()
+                if entry_urls is None:
+                    ipdb.set_trace()
+                for homepage in entry_urls:
+                    req = scrapy.Request(homepage, callback=self.parse_homepage, meta={'old_db_entry': db_entry})
+                    yield req
 
     def parse_homepage(self, response):
+        # push back all the urls
+        import ipdb
+        ipdb.set_trace()
+        # find if export // languages
+        # langues = languages_in_page
+        # fom angelList only
+        for url in response.css("a").filter(response.url.baseurl):
+            if url.baseurl == response.baseurl:
+                yield scrapy.Request(url, callback=self.parse)
+
         item = Business()
-        item['title'] = json.dumps(response.xpath("//title/text()").extract())
+        item['title'] = self.get_name(response)
         item['homepage'] = response.url
         item['meta_description'] = json.dumps(response.xpath("//meta[@name='description']/@content").extract())
         item['meta_keywords'] = json.dumps(response.xpath("//meta[@name='keywords']/@content").extract())
-        item['page_text'] = "" #  not used for now
+        item['page_text'] = ""  # not used for now
         item['dmoz_url'] = response.meta['dmoz_url']
         yield item
+
+    def get_name(self, response):
+        try:
+            response.xpath("footer -> title").extract()
+            response.xpath("//title/text()").extract()
+        except:
+            response.xpath("fallback to pg").extract()
