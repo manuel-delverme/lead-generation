44532408b653c9032313bf7eefcc740c3a629070
nysquit <nysquit@cthulu.de>
2016-05-22 16:23:50 +0200

    first part of search engine backend

--- webui/search.py
+++ webui/search.py
@@ -1,3 +1,253 @@
-connect_string = "dbname={} user={} host={} password={}".format("grepr", "spider", "tuamadre.net", DATABASE['password'])
-self._conn = psycopg2.connect(connect_string)
+# -*- coding: utf-8 -*-
+import json
+import re
+import psycopg2
+import psycopg2.extras
+import itertools
+from settings import DATABASE
+# import langdetect
+# import description_scraper.isocodes
+# import scrapy.utils.url
+# from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+# from description_scraper.items import Company
 
+
+class OutOfUrls(Exception):
+    pass
+
+def clean_urls(dirty_urls):
+    def remove_serialization(url):
+        if url[0] == '"' and url[-1] == '"':
+            url = url[1:-1]
+
+        if url[0] == '{' and url[-1] == '}':
+            url = url[1:-1]
+        return url
+
+    try:
+        entry_urls = json.loads(dirty_urls)
+    except ValueError:
+        entry_urls = remove_serialization(dirty_urls)
+
+        if "," in entry_urls:
+            entry_urls = dirty_urls.split(",")
+        else:
+            entry_urls = [dirty_urls]
+    else:
+        if type(entry_urls) in (unicode, str):
+            entry_urls = [entry_urls]
+        elif type(entry_urls) == list:
+            pass
+        else:
+            if "entry_urls is fucked up" not in self.ignoreBreakpoint:
+                print("entry_urls is fucked up")
+
+    if entry_urls is None:
+        if "entry_urls is None" not in self.ignoreBreakpoint:
+            import ipdb
+            ipdb.set_trace()
+            print("entry_urls is None")
+
+    entry_urls = [remove_serialization(url) for url in entry_urls]
+    return entry_urls
+
+
+def parse(self, response):
+    # push back all the urls
+    # find if export // languages
+    # langues = languages_in_page
+    # fom angelList only
+
+    if not hasattr(response, 'body_as_unicode'):
+        yield None
+    homepage_url = self.clean_urls(response.meta['old_db_entry']['homepage'])[0]
+    in_links_extractor = LinkExtractor(allow=(homepage_url), unique=True)
+    for link in in_links_extractor.extract_links(response):
+        yield scrapy.Request(link.url, callback=self.parse, meta=response.request.meta)
+
+    item = Company()
+    item['formal_name'] = self.get_name(response)
+    item['languages'] = self.get_languages(response)
+
+    item['peer_companies'] = self.peer_companies(response)
+    item['crawled_url'] = response.url
+    item['depth'] = response.meta['depth']
+    item['description'] = self.get_description(response)
+    item['keywords'] = self.get_keywords(response)
+
+    forward_values = [
+        'homepage',
+        'addresses',
+        'revenue',
+        'phones',
+        'employees_max',
+        # 'common_name',
+        'funding',
+        'zip',
+        # 'formal_name',
+        'angel_id',
+        'employees_min',
+        'angel_link',
+        'province',
+        'cities',
+        'emails',
+        'countries',
+        'pg_id'
+    ]
+    for key in forward_values:
+        item[key.replace("angel", "al")] = response.request.meta['old_db_entry'][key]
+    yield item
+
+def get_description(self, response):
+    # TODO add og, twitter etc
+    return json.dumps(
+        response.xpath("//meta[@name='description']/@content").extract()
+    )
+
+def get_keywords(self, response):
+    # TODO add og, twitter etc
+    return json.dumps(
+        response.xpath("//meta[@name='keywords']/@content").extract()
+    )
+
+def peer_companies(self, response):
+    # www.site.*
+    netloc = scrapy.utils.url.parse_url(response.url).netloc
+    basedomain = netloc[:netloc.rindex(".")]
+    # TODO: check DNS
+    return [basedomain + tld for tld in self.all_tld_domains(response)[0]]
+
+def all_tld_domains(self, response):
+
+    out_links_extractor = LinkExtractor(deny=(response.meta['old_db_entry']['homepage']), unique=True)
+    tlds = []
+    not_tlds = []
+    netloc = scrapy.utils.url.parse_url(response.url).netloc
+    basedomain = netloc[:netloc.rindex(".")]
+
+    for link in out_links_extractor.extract_links(response):
+        if "." in link.url:
+            base_url = link.url[:link.url.rindex(".")]
+            if base_url == basedomain:
+                tlds.append(netloc.split(".")[-1])
+            else:
+                not_tlds.append(netloc)
+    return tlds, set(not_tlds)
+
+def get_languages(self, response):
+    languages = set()
+    tlds, _ = self.all_tld_domains(response)
+    languages.update(tlds)
+
+    query_string = scrapy.utils.url.parse_url(response.url).query
+    query = scrapy.utils.url.parse_qsl(query_string)
+    for _, val in query:
+        if val in description_scraper.isocodes.languages:
+            languages.add(val)
+
+    # "?lang=en"
+    # TODO this should be on all the text
+    page_txt = ''.join(self.get_page_text(response))
+    try:
+        detected_languages = langdetect.detect_langs(page_txt)
+    except langdetect.lang_detect_exception.LangDetectException:
+        return languages
+    else:
+        for lang in detected_languages:
+            if lang.prob > 0.70:
+                languages.add(lang.lang)
+
+    return languages
+
+def get_page_text(self, response):
+    try:
+        return response.meta['page_text']
+    except KeyError:
+        soup = BeautifulSoup(response.body)
+        for elem in soup.findAll(['script', 'style']):
+            elem.extract()
+        text = map(unicode.lower, set(soup.getText().split("\n")))
+        response.meta['page_text'] = text
+        return text
+
+def get_name(self, response):
+    name = None
+    txt_arr = self.get_page_text(response)
+    clean_txt = [re.sub(r'[^a-zA-Z0-9.\+\-]+', ' ', m) for m in txt_arr]
+    # piva_matches = [txt for txt in txt_arr if re.search("p\.{0,1}\s*iva", txt) is not None]
+    for txt in clean_txt:
+        # piva_re = re.search("p\.{0,1}\s*iva\s*[0-9]+", match)
+        # piva = piva_re.group()
+        # code = int(re.search("[0-9]+", piva).group())
+        # match = match[:piva.start()] + match[piva.end():]
+        for company_type_regex in self.company_type_regexes:
+            name_match = re.search("[\s\w\-\!]+" + company_type_regex, txt)
+            if name_match:
+                name = name_match.group()
+                break
+        if name:
+            break
+
+    if not name:
+        try:
+            last_name = response.meta['old_db_entry']['common_name']
+        except Exception as e:
+            if "failed to find in html and no record present" not in self.ignoreBreakpoint:
+                import ipdb; ipdb.set_trace()
+                print("failed to find in html and no record present", e)
+        if last_name:
+            # check if the old name is valid
+            for company_type_regex in self.company_type_regexes:
+                name_match = re.search("[\s\w\-\!]+" + company_type_regex, last_name)
+                if name_match:
+                    name = name_match.group()
+                    break
+        if last_name and not name:
+            name = last_name
+        if not name:
+            if "failed to find in html and no record present" not in self.ignoreBreakpoint:
+                import ipdb; ipdb.set_trace()
+                print("last name is not present fallback to title")
+            name = response.xpath("//title/text()").extract()
+    return name
+
+
+def fetch_rows(_conn, lower, batch_size):
+    with _conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
+        cursor.execute( """
+            SELECT homepage, languages
+            FROM "Company"
+            WHERE languages LIKE '%it%' and languages LIKE '%en%'
+                AND formal_name ~ '%\ss[\.\s]*r[\.\s]*l[\.\s]*%'"""
+            #    ORDER BY id OFFSET %(lower)s LIMIT %(size)s;""",
+            #{'lower': lower, 'size': batch_size}
+        )
+        import ipdb; ipdb.set_trace()
+        if not cursor.rowcount:
+            raise OutOfUrls
+        for db_entry in cursor:
+            entry_urls = clean_urls(db_entry['homepage'])
+            for homepage in entry_urls:
+                if '"' in homepage:
+                    if "bad formatting" not in self.ignoreBreakpoint:
+                        import ipdb; ipdb.set_trace()
+                        print("bad formatting")
+                netloc = scrapy.utils.url.parse_url(homepage).netloc
+                if not netloc:
+                    homepage = "http://" + homepage
+                req = scrapy.Request(homepage, callback=self.parse, meta={'old_db_entry': db_entry})
+                yield req
+
+connect_string = "dbname={} user={} host={} password={}".format("grepr", "spider", "localhost", DATABASE['password'])
+_conn = psycopg2.connect(connect_string)
+
+batch_size = 100
+for step in itertools.count():
+    lower = step * batch_size
+    try:
+        for request in fetch_rows(_conn, lower, batch_size):
+            print(request)
+    except OutOfUrls:
+        print("DONE!")
+        break
---
+++ webui/settings.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for description_scraper project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'description_scraper'
+
+SPIDER_MODULES = ['description_scraper.spiders']
+NEWSPIDER_MODULE = 'description_scraper.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+USER_AGENT = "Googlebot/2.1 ( http://www.google.com/bot.html)"
+
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 30
+
+# Configure a delay for requests for the same website (default: 0)
+# See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+# DOWNLOAD_DELAY = 1
+
+# The download delay setting will honor only one of:
+CONCURRENT_REQUESTS_PER_DOMAIN = 3
+# CONCURRENT_REQUESTS_PER_IP = 1
+
+# Disable cookies (enabled by default)
+# COOKIES_ENABLED=False
+# COOKIES_DEBUG=True
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED=False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8,it;q=0.6',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+}
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    'description_scraper.middlewares.MyCustomSpiderMiddleware': 543,
+# }
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+}
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#    'scrapy.telnet.TelnetConsole': None,
+# }
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'description_scraper.pipelines.DatabasePipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See http://doc.scrapy.org/en/latest/topics/autothrottle.html
+# NOTE: AutoThrottle will honour the standard settings for concurrency and delay
+AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+AUTOTHROTTLE_START_DELAY = 0.10
+# The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 60
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG=False
+
+# Enable and configure HTTP caching (disabled by default)
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+DOWNLOAD_HANDLERS = {'s3': None}
+# DUPEFILTER_DEBUG = True
+TOR_PASSWORD = "howdoicake?"
+LOG_LEVEL = "INFO"
+LOG_FILE = "/tmp/scrapy.log"
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'spider',
+    'password': '666permission',
+    'database': 'grepr'
+}
+
+DEPTH_LIMIT = 1
+# DEPTH_PRIORITY = -1
